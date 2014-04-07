27e1a845e504353b7176134fda895583a7a1a7dd
Ahmed Jafri <ajafri19@gmail.com>
2014-04-06 23:44:07 -0700

    Restructuring code to implicitly declare DFF within STATE module

--- Lab1/STATE.v
+++ Lab1/STATE.v
@@ -12,10 +12,13 @@ module STATE(
   reset
   );
 
-  output reg increment, latch, dataValid;
-  output reg chipSelect, outputEnable, readEnable;
+  output increment, latch, dataValid;
+  output chipSelect, outputEnable, readEnable;
 
-  input read, write, clock, reset;
+  reg increment, latch, dataValid;
+  reg chipSelect, outputEnable, readEnable;
+
+  input wire read, write, clock, reset;
 
   parameter [3:0] R1 = 4'd0, R2 = 4'd1, R3 = 4'd2; 
   parameter [3:0] R4 = 4'd3, R5 = 4'd4, R6 = 4'd5; 
@@ -23,11 +26,19 @@ module STATE(
   parameter [3:0] W1 = 4'd6, W2 = 4'd7, W3 = 4'd8; 
   parameter [3:0] W4 = 4'd9, W5 = 4'd10, W6 = 4'd11; 
 
-  wire  [3:0] PS; 
+  reg  [3:0] PS; 
   reg   [3:0] NS;
 
 
-  always @(PS) 
+  always @(posedge clock or posedge reset)
+    begin
+    if (reset) 
+      PS = R1;  // On reset, set to 0 
+    else 
+      PS = NS; // Otherwise out = d 
+    end
+
+  always @(PS or reset or read or write) 
     case(PS)
       R1: if(read) NS = R2;
           else     NS = W1;
@@ -36,7 +47,7 @@ module STATE(
       R4: NS = R5;
       R5: NS = R6;
       R6: NS = R1;
-
+     
       W1: if(write) NS = W2;
           else      NS = R1;
       W2: NS = W3;
@@ -44,7 +55,7 @@ module STATE(
       W4: NS = W5;
       W5: NS = W6;
       W6: NS = W1;
-
+      default: NS = R1;
     endcase
 
   always @(PS)
@@ -54,132 +65,109 @@ module STATE(
     case(PS)
       R1: 
       begin  
-          increment     = 0;
-          readEnable    = 1;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 1;
+        increment     = 0; 
+        readEnable    = 1;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 1;
       end
-
       R2:
       begin
-          increment     = 1;
-          readEnable    = 1;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 1;
+        increment     = 1;
+        readEnable    = 1;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 1;
       end 
-
       R3:
       begin
-          increment     = 0;
-          readEnable    = 1;
-          latch         = 1;
-          outputEnable  = 1;
-          dataValid     = 1;
+        increment     = 0;
+        readEnable    = 1;
+        latch         = 1;
+        outputEnable  = 1;
+        dataValid     = 1;
       end
-
       R4: 
       begin
-          increment     = 0;
-          readEnable    = 1;
-          latch         = 0;
-          outputEnable  = 0;
-          dataValid     = 1;
+        increment     = 0;
+        readEnable    = 1;
+        latch         = 0;
+        outputEnable  = 0;
+        dataValid     = 1;
       end
-
       R5: 
       begin
-          increment     = 0;
-          readEnable    = 1;
-          latch         = 0;
-          outputEnable  = 0;
-          dataValid     = 0;
+        increment     = 0;
+      readEnable    = 1;
+        latch         = 0;
+        outputEnable  = 0;
+        dataValid     = 0;
       end
-
       R6: 
       begin
-          increment     = 0;
-          readEnable    = 1;
-          latch         = 0;
-          outputEnable  = 0;
-          dataValid     = 1;
+        increment     = 0;
+        readEnable    = 1;
+        latch         = 0;
+        outputEnable  = 0;
+        dataValid     = 1;
       end
-
       W1: 
       begin
-          increment     = 0;
-          readEnable    = 0;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 0;
+        increment     = 0;
+        readEnable    = 0;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 0;
       end
-
       W2: 
       begin
-          increment     = 1;
-          readEnable    = 0;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 0;
+        increment     = 1;
+        readEnable    = 0;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 0;
       end
 
       W3: 
       begin
-          increment     = 0;
-          readEnable    = 0;
-          latch         = 1;
-          outputEnable  = 1;
-          dataValid     = 0;
+        increment     = 0;
+        readEnable    = 0;
+        latch         = 1;
+        outputEnable  = 1;
+        dataValid     = 0;
       end
-
       W4: 
       begin
-          increment     = 0;
-          readEnable    = 0;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 0;
+        increment     = 0;
+        readEnable    = 0;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 0;
       end
-
       W5: 
       begin
-          increment     = 0;
-          readEnable    = 0;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 1;
+        increment     = 0;
+        readEnable    = 0;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 1;
       end
-
       W6: 
       begin
-          increment     = 0;
-          readEnable    = 0;
-          latch         = 0;
-          outputEnable  = 1;
-          dataValid     = 0;
+        increment     = 0;
+        readEnable    = 0;
+        latch         = 0;
+        outputEnable  = 1;
+        dataValid     = 0;
       end
-
+      default:
+      begin
+      increment     = 0;
+        readEnable    = 0;
+        latch         = 0;
+        outputEnable  = 0;
+        dataValid     = 0;
+      end
     endcase
 
-D_FF d1 (PS[0], NS[0], reset, clock);
-D_FF d2 (PS[1], NS[1], reset, clock);
-D_FF d3 (PS[2], NS[2], reset, clock);
-D_FF d4 (PS[3], NS[3], reset, clock);
-
 endmodule
-
-
-module D_FF (q, d, reset, clk); 
-
-  output q; 
-  input d, reset, clk; 
-  reg q; // Indicate that q is stateholding  
-    
-  always @(posedge clk or posedge reset) 
-    if (reset) 
-      q = 0;  // On reset, set to 0 
-    else 
-      q = d; // Otherwise out = d 
-      
-endmodule 
