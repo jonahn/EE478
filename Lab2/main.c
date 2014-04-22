07a3005e5f567a477e63c778ea7619f1ec2902b1
Ahmed Jafri <ajafri19@gmail.com>
2014-04-22 15:35:38 -0700

    Adding main.c for Lab2

---
+++ Lab2/main.c
@@ -0,0 +1,17 @@
+// 
+// Remote Surgery System 
+// main.c
+// 
+// Ahmed Jafri, Anna Horstmann, Jake Yang
+//
+
+#pragma config FOSC = ECHPIO6	// Select external oscillator (EC) - high power
+#pragma config PLLCFG = OFF		// Using oscillator directly.
+#pragma config PRICLKEN = ON	// Primary Clock Enable
+#pragma config FCMEN = OFF 		// Fail Safe Clock Monitor Enable
+#pragma config IESO = OFF		// External/Internal Oscillator Switchover
+
+void main()
+{
+
+}
