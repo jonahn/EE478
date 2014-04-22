5cf3cfda6e73378447f2fc06a350a5569c4c223c
Ahmed Jafri <ajafri19@gmail.com>
2014-04-22 15:54:56 -0700

    Adding interrupt service routine

--- Lab2/main.c
+++ Lab2/main.c
@@ -11,7 +11,35 @@
 #pragma config FCMEN = OFF 		// Fail Safe Clock Monitor Enable
 #pragma config IESO = OFF		// External/Internal Oscillator Switchover
 
+//interrupt setup
+#pragma code
+#pragma interruptlow InterruptHandlerLow
+
+char buffer;
+
 void main()
 {
 
+}
+
+#pragma code InterruptVectorLow = 0x08
+void
+InterruptVectorLow()
+{
+  _asm
+    goto InterruptHandlerLow  //jump to interrupt routine
+  _endasm
+}
+
+
+
+void
+InterruptHandlerLow()
+{
+	if(PIR1bits.RCIF == 1) 
+    {
+     	buffer = Read1USART();
+    }
+
+    PIR1bits.RCIF = 0;
 }
