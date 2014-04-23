a1d6d86c6571f7a770f42873787f8a8be57cabfa
Ahmed Jafri <ajafri19@gmail.com>
2014-04-22 19:59:43 -0700

    Cleaning up git repository
    Remove all files that are not source or header files.

--- Lab2/test.X/build/default/debug/Main.o.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/Main.o:  \
-	C:/Users/ajafri19/Documents/GitHub/EE478/Lab2/test.X/Main.c  \
-	C:/Users/ajafri19/Documents/GitHub/EE478/Lab2/test.X/uart_interrupts.h  \
-
--- Lab2/test.X/build/default/debug/uart_interrupts.o.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/uart_interrupts.o:  \
-	C:/Users/ajafri19/Documents/GitHub/EE478/Lab2/test.X/uart_interrupts.c  \
-	C:/Users/ajafri19/Documents/GitHub/EE478/Lab2/test.X/uart_interrupts.h  \
-
