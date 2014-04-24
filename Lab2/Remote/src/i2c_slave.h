9fdbef0deaeb57090c326bba356aa328a4ba414e
JakeRawr <tsunghan614@gmail.com>
2014-04-23 21:16:35 -0700

    Added i2c slave.c .h

---
+++ Lab2/Remote/src/i2c_slave.h
@@ -0,0 +1,17 @@
+//
+//  Lab2_Slave.h
+//  
+//
+//  Created by Jake Yang on 4/23/14.
+//
+//
+
+#ifndef _i2c_slave_h
+#define _i2c_slave_h
+    
+// Function prototypes for the high priority ISRs
+void highPriorityISR(void);
+    
+
+
+#endif
