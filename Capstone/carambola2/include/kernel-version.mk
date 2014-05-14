a74aab79d42c15f15c18d01132038ee63b3abde9
Ahmed Jafri <ajafri19@gmail.com>
2014-05-13 17:10:47 -0700

    Adding carambola2 folder since symlink couldn't be followed

---
+++ Capstone/carambola2/include/kernel-version.mk
@@ -0,0 +1,30 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+LINUX_RELEASE?=1
+
+ifeq ($(LINUX_VERSION),3.3.8)
+  LINUX_KERNEL_MD5SUM:=f1058f64eed085deb44f10cee8541d50
+endif
+ifeq ($(LINUX_VERSION),3.6.11)
+  LINUX_KERNEL_MD5SUM:=3d602ad7f7984509c3e923a5ae90bc54
+endif
+ifeq ($(LINUX_VERSION),3.7.9)
+  LINUX_KERNEL_MD5SUM:=c19426374acfccfa7391fc90640240cb
+endif
+ifeq ($(LINUX_VERSION),3.8)
+  LINUX_KERNEL_MD5SUM:=1c738edfc54e7c65faeb90c436104e2f
+endif
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM?=x
+
+split_version=$(subst ., ,$(1))
+merge_version=$(subst $(space),.,$(1))
+KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+ifeq ($(firstword $(call split_version,$(KERNEL_BASE))),2)
+  KERNEL_PATCHVER=$(call merge_version,$(wordlist 1,3,$(call split_version,$(KERNEL_BASE))))
+else
+  KERNEL_PATCHVER=$(KERNEL)
+endif
+
