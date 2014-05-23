130e9873e7b6c78c6de3954c20748217f4f412e0
JakeRawr <tsunghan614@gmail.com>
2014-05-23 13:13:29 -0700

    SPI_M4
    SPI read and write back successful

--- Capstone/Project/main.c
+++ Capstone/Project/main.c
@@ -1,14 +1,19 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
+#include <stm32f4xx_spi.h>
 
-#include "setupSDIO.h"
 
 extern void setupSDIO();
-
+extern uint8_t mySPI_GetData(uint8_t);
+extern void mySPI_Init(void);
+unsigned char data;
 unsigned char iteration;
 
-void main (void) {
+
+
+
+/*void main (void) {
   //for testing LED
   STM_EVAL_LEDInit(LED6);
   iteration = 0;
@@ -31,4 +36,31 @@ void main (void) {
     
     
   }
+}*/
+
+int main(void)
+{
+        //Configure the SPI 
+	mySPI_Init();
+      
+    while(1)
+    {
+        //Receive and Write back to Rpi, ignore 0x29
+    	mySPI_GetData(0x29);   
+    }
+
+}
+
+
+
+
+uint8_t mySPI_GetData(uint8_t adress){
+ 
+while(!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE));
+data = SPI_I2S_ReceiveData(SPI1); //Receive and store to Data
+ 
+while(!SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE)); 
+SPI_I2S_SendData(SPI1, 0x08);  // Write an indicator back
+ 
+return  0;
 }
