46b57696c3334695b058d625705ff2f2b456ae02
Ahmed Jafri <ajafri19@gmail.com>
2014-05-24 01:02:04 -0700

    vzx

--- Capstone/RaspberryPi/Bridg/src/main.c
+++ Capstone/RaspberryPi/Bridg/src/main.c
@@ -101,7 +101,7 @@ int main(int argc, char **argv)
             int frameSize = 256;
             int i;
             
-            uint32_t dataLength = 88000;
+            uint32_t dataLength = 60000;
             
             char mp3Data[dataLength];
             
