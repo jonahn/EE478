74b83047ccc376df495f25fbcb6d5dd42669408c
annahorstmann <annahorstmann@hotmail.com>
2014-06-05 20:31:46 -0700

    debugpi

--- Capstone/RaspberryPi/Bridg/src/main.cpp
+++ Capstone/RaspberryPi/Bridg/src/main.cpp
@@ -171,8 +171,8 @@ int main(int argc, char **argv)
                         playListSize = reciever.files->size() + '0';
                         wiringPiI2CWrite (fd, playListSize);
 
-                        percentPlayed = ( /*currentIndex / */currentFile.totalSongLength) + '0';
-                        wiringPiI2CWrite (fd, percentPlayed);
+                       // percentPlayed = ( /*currentIndex / */currentFile.totalSongLength) + '0';
+                       // wiringPiI2CWrite (fd, percentPlayed);
 
                         //for (currentFile.songArtist)
                             
